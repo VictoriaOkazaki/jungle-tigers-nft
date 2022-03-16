2b4189cc293d147780525b635ceb7ef6ed5dee2b
VictoriaOkazaki <victoria.timein@gmail.com>
2022-03-16 11:50:32 +0400

    VIDEO AUTOPLAY ADDED, loader time set 5s

--- app/js/loader.js
+++ app/js/loader.js
@@ -2,7 +2,7 @@ const imgs = document.images;
 const imgsLen = imgs.length;
 let loadImgCount = 0;
 
-window.addEventListener('load', (event) => {
+document.addEventListener('DOMContentLoaded', (event) => {
     [].forEach.call(imgs, function(img) {
         if(img.complete) {
             incrementImgCounter();
@@ -13,13 +13,24 @@ window.addEventListener('load', (event) => {
     setTimeout(hideLoader, 5000);
 });
 
+
 let loaderVisible = true;
 function hideLoader() {
     if (!loaderVisible) return;
-    loaderVisible = true;
+    loaderVisible = false;
     // console.log('All images on page loaded');
     document.querySelector('.loader-wrapper').style.display = 'none';
     document.querySelector('.wrapper').style.visibility = 'visible';
+    // const video = document.querySelector('.project__video');
+    // console.log('video start load');
+    // video.load();
+    // const start = performance.now();
+    // video.oncanplaythrough = () => {
+    //     video.oncanplaythrough = null;
+    //     const end = performance.now();
+    //     console.log('video loaded', end - start, 'ms');
+    //     video.play();
+    // };
 }
 
 function incrementImgCounter() {
